dca034ddf995050acfa06463d328a191aa3512a6
Zeinab <zeinabansarian6280@gmail.com>
2024-12-17 12:22:01 +0330

    dynamic product page

--- js/product.js
+++ js/product.js
@@ -1,26 +1,21 @@
-const thumbsSlider = new Swiper('.thumbsSlider', {
-    // Optional parameters
-    loop: true,
-  
-  speed:1000,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.nextThumb',
-      prevEl: '.prevThumb',
-    },
-    slidesPerView: 3,
-    spaceBetween: 20,
-  
- 
-  });
+const thumbsSlider = new Swiper(".thumbsSlider", {
+  // Optional parameters
+  loop: true,
 
+  speed: 1000,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".nextThumb",
+    prevEl: ".prevThumb",
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+});
 
-
-
-  const imgsSlider = new Swiper('.imgsSlider', {
-    speed:1000,
-    loop: true,
-    thumbs: {
-        swiper: thumbsSlider
-      }
-  });
+const imgsSlider = new Swiper(".imgsSlider", {
+  speed: 1000,
+  loop: true,
+  thumbs: {
+    swiper: thumbsSlider,
+  },
+});
