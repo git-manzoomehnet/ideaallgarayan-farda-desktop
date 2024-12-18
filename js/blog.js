78bec6c3443d1aeb4e799241057a485ed3d5d71c
faezeh <faezeh0078@gmail.com>
2024-12-18 14:55:37 +0330

    changes

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,17 @@
+const host = {
+  debug: !1,
+  settings: {
+    'connection.web.callcommand': '/',
+      "connection.web.trust_login": "https://basispanel.ir/apicms",
+      "connection.web.basiscore": "https://basispanel.ir/apicms",
+      "connection.web.media": "https://basispanel.ir/apicms",
+      "default.dbsource.verb": "post",
+      "default.call.verb": "get",
+      "default.viewCommand.groupColumn": "prpid",
+      "default.dmnid": "4914",
+      "default.binding.regex": "\\{##([^#]*)##\\}",
+  },
+};
 let filtersBtn = [...document.querySelectorAll('.Filters .btn')]
 let catid
 filtersBtn.forEach((btn,i)=>{
@@ -6,38 +20,37 @@ filtersBtn.forEach((btn,i)=>{
     }
 
 })
-
 let loaderContainer = document.querySelector(".loaderContainer")
-$(document).ready(function () {
-  $(".Filters .btn").click(function (e) {
-    catid = e.currentTarget.getAttribute('data-catid')
-      var url4 = `/load-blogDesk.inc?catid=${catid}`;
-      filtersBtn.forEach((btn,i)=>{
-      filtersBtn.map(b=>{
-                return b.classList.remove('active')
-            })
-          })
-      e.currentTarget.classList.add('active')
-    loaderContainer.style.display="flex"
-      $.ajax({
-        url: url4,
-        method: 'GET',
-        success: function(response) {
-            // مخفی کردن لودینگ بعد از دریافت پاسخ
-            $(".blogList").html(response);
-            setTimeout(() => {
-              gsap.to('.blog',{
-                opacity:1,
-                y:0,
-               stagger:.1
-               })
-            }, 300)
-             loaderContainer.style.display="none"
-        },
-       
-    });
-  });
-});
+filtersBtn.forEach(btn=>{
+  btn.addEventListener('click',(e)=>{
+    filtersBtn.map(b=>{
+      return b.classList.remove('active')
+   
+  })
+  e.currentTarget.classList.add('active')
+  catid = e.currentTarget.getAttribute('data-catid')
+  $bc.setSource('btn.load',true)
+  $bc.setSource('btn.catid',catid)
+  })
+})
+function fn(args){
+  loaderContainer.style.display="flex"
+  console.log('proc',args.result);
+  let prevblogs =[...document.querySelectorAll('.blogList .blog')] 
+  prevblogs.map(blog=> blog.remove())
+}
+function render(args){
+  loaderContainer.style.display="none"
+  console.log('render',args);
+  setTimeout(() => {
+                  gsap.to('.blog',{
+                    opacity:1,
+                    y:0,
+                   stagger:.1
+                   })
+                }, 300)
+ 
+}
 
 let blogs = document.querySelectorAll('.blog')
 blogs.forEach(blog=>{
